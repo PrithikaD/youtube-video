5b47e6ddb2bbe1187e9c4d38330f7012c64cfee9
PrithikaD <prithika.devarajan@gmail.com>
2026-02-05 15:16:54 -0500

    hopefully this works

--- app/login/login-form.tsx
+++ app/login/login-form.tsx
@@ -27,6 +27,7 @@ export default function LoginForm({
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
+        credentials: "include",
         body: JSON.stringify({ email, password }),
       }
     );
